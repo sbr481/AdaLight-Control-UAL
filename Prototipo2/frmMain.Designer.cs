9034bf0e7fddc4f37fc782ae060a4b7a2efd09fc
Alfonso <Alfonso@Laptop>
2017-01-21 22:59:22 +0100

    Agregar archivos de proyecto.

---
+++ Prototipo2/frmMain.Designer.cs
@@ -0,0 +1,234 @@
+﻿using System;
+
+namespace Prototipo2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btPuerto = new System.Windows.Forms.Button();
+            this.btNLEDS = new System.Windows.Forms.Button();
+            this.cbSelecLED = new System.Windows.Forms.ComboBox();
+            this.btColorTira = new System.Windows.Forms.Button();
+            this.tbNLEDS = new System.Windows.Forms.TextBox();
+            this.tbPuerto = new System.Windows.Forms.TextBox();
+            this.lbNLEDS = new System.Windows.Forms.Label();
+            this.lbPuerto = new System.Windows.Forms.Label();
+            this.lbColorTira = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.lbRGBTira = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbColorLED = new System.Windows.Forms.Label();
+            this.tbColorTira = new System.Windows.Forms.TextBox();
+            this.tbColorLED = new System.Windows.Forms.TextBox();
+            this.lbRGBLED = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btPuerto
+            // 
+            this.btPuerto.Location = new System.Drawing.Point(683, 362);
+            this.btPuerto.Name = "btPuerto";
+            this.btPuerto.Size = new System.Drawing.Size(89, 23);
+            this.btPuerto.TabIndex = 0;
+            this.btPuerto.Text = "Puerto";
+            this.btPuerto.UseVisualStyleBackColor = true;
+            this.btPuerto.Click += new System.EventHandler(this.btPuerto_Click);
+            // 
+            // btNLEDS
+            // 
+            this.btNLEDS.Location = new System.Drawing.Point(683, 386);
+            this.btNLEDS.Name = "btNLEDS";
+            this.btNLEDS.Size = new System.Drawing.Size(89, 23);
+            this.btNLEDS.TabIndex = 1;
+            this.btNLEDS.Text = "N LEDS";
+            this.btNLEDS.UseVisualStyleBackColor = true;
+            this.btNLEDS.Click += new System.EventHandler(this.btNLEDS_Click);
+            // 
+            // cbSelecLED
+            // 
+            this.cbSelecLED.FormattingEnabled = true;
+            this.cbSelecLED.Location = new System.Drawing.Point(119, 290);
+            this.cbSelecLED.Name = "cbSelecLED";
+            this.cbSelecLED.Size = new System.Drawing.Size(121, 24);
+/*            int numLEDs = Int32.Parse(tbNLEDS.Text);
+            this.cbSelecLED.TabIndex = Int32.Parse(tbNLEDS.Text);
+*/          this.cbSelecLED.TabIndex = 2;
+            this.cbSelecLED.SelectedIndexChanged += new System.EventHandler(this.cbSelecLED_SelectedIndexChanged);
+//            this.cbSelecLED.It
+            // 
+            // btColorTira
+            // 
+            this.btColorTira.Location = new System.Drawing.Point(683, 413);
+            this.btColorTira.Name = "btColorTira";
+            this.btColorTira.Size = new System.Drawing.Size(89, 23);
+            this.btColorTira.TabIndex = 3;
+            this.btColorTira.Text = "Color Tira";
+            this.btColorTira.UseVisualStyleBackColor = true;
+            this.btColorTira.Click += new System.EventHandler(this.btColorTira_Click);
+            // 
+            // tbNLEDS
+            // 
+            this.tbNLEDS.Location = new System.Drawing.Point(176, 66);
+            this.tbNLEDS.Name = "tbNLEDS";
+            this.tbNLEDS.Size = new System.Drawing.Size(59, 22);
+            this.tbNLEDS.TabIndex = 5;
+//            this.tbNLEDS.TextChanged += new System.EventHandler(this.tbNLEDS_TextChanged);
+//            this.tbNLEDS.Text = "0";
+            // tbPuerto
+            // 
+            this.tbPuerto.Location = new System.Drawing.Point(176, 36);
+            this.tbPuerto.Name = "tbPuerto";
+            this.tbPuerto.Size = new System.Drawing.Size(59, 22);
+            this.tbPuerto.TabIndex = 6;
+            this.tbPuerto.TextChanged += new System.EventHandler(this.tbPuerto_TextChanged);
+            // 
+            // lbNLEDS
+            // 
+            this.lbNLEDS.AutoSize = true;
+            this.lbNLEDS.Location = new System.Drawing.Point(78, 69);
+            this.lbNLEDS.Name = "lbNLEDS";
+            this.lbNLEDS.Size = new System.Drawing.Size(63, 17);
+            this.lbNLEDS.TabIndex = 8;
+            this.lbNLEDS.Text = "Nº LEDS";
+            // 
+            // lbPuerto
+            // 
+            this.lbPuerto.AutoSize = true;
+            this.lbPuerto.Location = new System.Drawing.Point(78, 39);
+            this.lbPuerto.Name = "lbPuerto";
+            this.lbPuerto.Size = new System.Drawing.Size(50, 17);
+            this.lbPuerto.TabIndex = 9;
+            this.lbPuerto.Text = "Puerto";
+            // 
+            // lbColorTira
+            // 
+            this.lbColorTira.AutoSize = true;
+            this.lbColorTira.Location = new System.Drawing.Point(78, 99);
+            this.lbColorTira.Name = "lbColorTira";
+            this.lbColorTira.Size = new System.Drawing.Size(70, 17);
+            this.lbColorTira.TabIndex = 10;
+            this.lbColorTira.Text = "Color Tira";
+            // 
+            // lbRGBTira
+            // 
+            this.lbRGBTira.AutoSize = true;
+            this.lbRGBTira.Location = new System.Drawing.Point(204, 99);
+            this.lbRGBTira.Name = "lbRGBTira";
+            this.lbRGBTira.Size = new System.Drawing.Size(41, 17);
+            this.lbRGBTira.TabIndex = 11;
+            this.lbRGBTira.Text = "black";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 293);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "LED";
+            // 
+            // lbColorLED
+            // 
+            this.lbColorLED.AutoSize = true;
+            this.lbColorLED.Location = new System.Drawing.Point(246, 293);
+            this.lbColorLED.Name = "lbColorLED";
+            this.lbColorLED.Size = new System.Drawing.Size(41, 17);
+            this.lbColorLED.TabIndex = 13;
+            this.lbColorLED.Text = "Color";
+            // 
+            // tbColorTira
+            // 
+            this.tbColorTira.Location = new System.Drawing.Point(176, 96);
+            this.tbColorTira.Name = "tbColorTira";
+            this.tbColorTira.Size = new System.Drawing.Size(22, 22);
+            this.tbColorTira.TabIndex = 14;
+            // 
+            // tbColorLED
+            // 
+            this.tbColorLED.Location = new System.Drawing.Point(293, 292);
+            this.tbColorLED.Name = "tbColorLED";
+            this.tbColorLED.Size = new System.Drawing.Size(22, 22);
+            this.tbColorLED.TabIndex = 15;
+            // 
+            // lbRGBLED
+            // 
+            this.lbRGBLED.AutoSize = true;
+            this.lbRGBLED.Location = new System.Drawing.Point(321, 295);
+            this.lbRGBLED.Name = "lbRGBLED";
+            this.lbRGBLED.Size = new System.Drawing.Size(40, 17);
+            this.lbRGBLED.TabIndex = 16;
+            this.lbRGBLED.Text = "white";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 447);
+            this.Controls.Add(this.lbRGBLED);
+            this.Controls.Add(this.tbColorLED);
+            this.Controls.Add(this.tbColorTira);
+            this.Controls.Add(this.lbColorLED);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbRGBTira);
+            this.Controls.Add(this.lbColorTira);
+            this.Controls.Add(this.lbPuerto);
+            this.Controls.Add(this.lbNLEDS);
+            this.Controls.Add(this.tbPuerto);
+            this.Controls.Add(this.tbNLEDS);
+            this.Controls.Add(this.btColorTira);
+            this.Controls.Add(this.cbSelecLED);
+            this.Controls.Add(this.btNLEDS);
+            this.Controls.Add(this.btPuerto);
+            this.Name = "frmMain";
+            this.Text = "frmMain";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btPuerto;
+        private System.Windows.Forms.Button btNLEDS;
+        private System.Windows.Forms.ComboBox cbSelecLED;
+        private System.Windows.Forms.Button btColorTira;
+        private System.Windows.Forms.TextBox tbNLEDS;
+        private System.Windows.Forms.TextBox tbPuerto;
+        private System.Windows.Forms.Label lbNLEDS;
+        private System.Windows.Forms.Label lbPuerto;
+        private System.Windows.Forms.Label lbColorTira;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label lbRGBTira;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbColorLED;
+        private System.Windows.Forms.TextBox tbColorTira;
+        private System.Windows.Forms.TextBox tbColorLED;
+        private System.Windows.Forms.Label lbRGBLED;
+    }
+}
+
---
+++ Prototipo2/frmMain.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototipo2
+{
+    public partial class frmMain : Form
+    {
+        System.IO.Ports.SerialPort ArduinoPort;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            //crear Serial Port
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = "COM3"; //sustituir por vuestro 
+            ArduinoPort.BaudRate = 115200;
+            ArduinoPort.Open();
+
+            //crear Serial Port
+            //            ArduinoPort = new System.IO.Ports.SerialPort();
+            //            ArduinoPort.PortName = "COM4";  //sustituir por vuestro 
+            //            ArduinoPort.BaudRate = 9600;
+            //            ArduinoPort.Open();
+
+            //vincular eventos
+            this.FormClosing += FrmMain_FormClosing;
+            this.btNLEDS.Click += btNLEDS_Click;
+            this.btPuerto.Click += btPuerto_Click;
+            this.tbColorTira.Click += tbColorTira_Click;
+            this.tbColorLED.Click += tbColorLED_Click;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //cerrar puerto
+            if (ArduinoPort.IsOpen) ArduinoPort.Close();
+        }
+
+
+        private void btPuerto_Click(object sender, EventArgs e)
+        {
+
+            byte[] serialData = new byte[31];
+
+            serialData[0] = 0x41;                              // Magic word
+            serialData[1] = 0x64;
+            serialData[2] = 0x61;
+
+
+            serialData[3] = (byte)((8 - 1) >> 8);   // LED count high byte
+            serialData[4] = (byte)((8 - 1) & 0xff); // LED count low byte
+            serialData[5] = (byte)(serialData[3] ^ serialData[4] ^ 0x55); // Checksum
+
+            /*
+                        // A special header / magic word is expected by the corresponding LED
+                        // streaming code running on the Arduino.  This only needs to be initialized
+                        // once (not in draw() loop) because the number of LEDs remains constant:
+                        serialData[0] = 'A';                              // Magic word
+                        serialData[1] = 'd';
+                        serialData[2] = 'a';
+                        serialData[3] = (byte)((leds.length - 1) >> 8);   // LED count high byte
+                        serialData[4] = (byte)((leds.length - 1) & 0xff); // LED count low byte
+                        serialData[5] = (byte)(serialData[3] ^ serialData[4] ^ 0x55); // Checksum
+
+            */
+
+
+
+
+
+
+            /*            serialData[6] = 0x56;
+                        serialData[7] = 0x00;
+                        serialData[8] = 0x00;
+                        serialData[9] = 0x2f;
+                        serialData[10] = 0x4e;
+                        serialData[11] = 0x4a;
+                        serialData[12] = 0x4f;
+                        serialData[13] = 0x5a;
+                        serialData[14] = 0x5a;
+                        serialData[15] = 0x5a;
+                        serialData[16] = 0x2d;
+                        serialData[17] = 0x2c;
+                        serialData[18] = 0x2c;
+                        serialData[19] = 0x4a;
+                        serialData[20] = 0x3f;
+                        serialData[21] = 0x49;
+                        serialData[22] = 0x58;
+                        serialData[23] = 0x57;
+                        serialData[24] = 0x00;
+                        serialData[25] = 0x00;
+                        serialData[26] = 0x00;
+                        serialData[27] = 0x00;
+                        serialData[28] = 0x00;
+                        serialData[29] = 0x00;
+                        serialData[30] = 0x00;
+            */
+
+
+
+
+            serialData[6] = 0x52;
+                        serialData[7] = 0x31;
+                        serialData[8] = 0x32;
+                        serialData[9] = 0x2f;
+                        serialData[10] = 0x4e;
+                        serialData[11] = 0x4a;
+                        serialData[12] = 0x4f;
+                        serialData[13] = 0x5a;
+                        serialData[14] = 0x5a;
+                        serialData[15] = 0x5a;
+                        serialData[16] = 0x2d;
+                        serialData[17] = 0x2c;
+                        serialData[18] = 0x2c;
+                        serialData[19] = 0x4a;
+                        serialData[20] = 0x3f;
+                        serialData[21] = 0x00;
+                        serialData[22] = 0x00;
+                        serialData[23] = 0x57;
+                        serialData[24] = 0x59;
+                        serialData[25] = 0x4c;
+                        serialData[26] = 0x4a;
+                        serialData[27] = 0x4d;
+                        serialData[28] = 0x58;
+                        serialData[29] = 0x57;
+                        serialData[30] = 0x5a;
+            
+
+
+
+
+            ArduinoPort.Write(serialData,0,31);
+            //            ArduinoPort.Write("b");
+        }
+
+        private void btNLEDS_Click(object sender, EventArgs e)
+        {
+
+
+
+
+
+
+
+            byte[] serialData = new byte[31];
+
+            serialData[0] = 0x41;                              // Magic word
+            serialData[1] = 0x64;
+            serialData[2] = 0x61;
+
+
+            serialData[3] = (8 - 1) >> 8;   // LED count high byte
+            serialData[4] = (8 - 1) & 0xff; // LED count low byte
+            serialData[5] = (byte)(serialData[3] ^ serialData[4] ^ 0x55); // Checksum
+
+
+            
+            serialData[6] = 0x52;
+            serialData[7] = 0x31;
+            serialData[8] = 0x32;
+            serialData[9] = 0x2f;
+            serialData[10] = 0x4e;
+            serialData[11] = 0x4a;
+            serialData[12] = 0x4f;
+            serialData[13] = 0x5a;
+            serialData[14] = 0x5a;
+            serialData[15] = 0x5a;
+            serialData[16] = 0x2d;
+            serialData[17] = 0x2c;
+            serialData[18] = 0x2c;
+            serialData[19] = 0x4a;
+            serialData[20] = 0x3f;
+            serialData[21] = 0x49;
+            serialData[22] = 0x58;
+            serialData[23] = 0x57;
+            serialData[24] = 0x59;
+            serialData[25] = 0x4c;
+            serialData[26] = 0x4a;
+            serialData[27] = 0x4d;
+            serialData[28] = 0x58;
+            serialData[29] = 0x57;
+            serialData[30] = 0x5a;
+
+
+
+
+
+            ArduinoPort.Write(serialData, 0, 31);
+
+
+
+
+
+
+
+
+
+
+            //            ArduinoPort.Write("a");
+        }
+
+        private void btColorTira_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void tbColorLED_Click(object sender, EventArgs e)
+        {
+            ColorDialog DialogColorLED = new ColorDialog();
+            // Keeps the user from selecting a custom color.
+            DialogColorLED.AllowFullOpen = true;
+            // Allows the user to get help. (The default is false.)
+            DialogColorLED.ShowHelp = false;
+            // Sets the initial color select to the current text color.
+            DialogColorLED.Color = tbColorLED.ForeColor;
+
+            // Update the text box color if the user clicks OK 
+            if (DialogColorLED.ShowDialog() == DialogResult.OK)
+            {
+                this.tbColorLED.BackColor = DialogColorLED.Color;
+                this.lbRGBLED.Text = "R = " + DialogColorLED.Color.R + ", G = " + DialogColorLED.Color.G + ", B = " + DialogColorLED.Color.B;
+            }
+        }
+
+
+        private void tbColorTira_Click(object sender, EventArgs e)
+        {
+            ColorDialog DialogColorTira = new ColorDialog();
+            // Keeps the user from selecting a custom color.
+            DialogColorTira.AllowFullOpen = true;
+            // Allows the user to get help. (The default is false.)
+            DialogColorTira.ShowHelp = false;
+            // Sets the initial color select to the current text color.
+            DialogColorTira.Color = tbColorLED.ForeColor;
+
+            // Update the text box color if the user clicks OK 
+            if (DialogColorTira.ShowDialog() == DialogResult.OK)
+            {
+                this.tbColorTira.BackColor = DialogColorTira.Color;
+//              tbColorTira.Text = "R = " + MyDialog.Color.R + ", G = " + MyDialog.Color.G + ", B = " + MyDialog.Color.B;
+                this.lbRGBTira.Text = "R = " + DialogColorTira.Color.R + ", G = " + DialogColorTira.Color.G + ", B = " + DialogColorTira.Color.B + "Lum" + DialogColorTira.Color.GetBrightness();
+            }
+
+        }
+
+        private void cbSelecLED_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbPuerto_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void tbColorLED_TextChanged(object sender, EventArgs e)
+        {
+//            
+        }
+
+        /*        private void label1_Click(object sender, EventArgs e)
+                {
+
+                }
+
+        *//*        private void label2_Click(object sender, EventArgs e)
+                {
+
+                }
+        */
+    }
+}
